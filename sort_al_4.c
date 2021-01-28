ea48ee088a2e49b0d80f43bb81dab26367aa8962
haninteger97 <intrager@naver.com>
2021-01-28 21:28:39 +0900

    sort 01/28_2
    2021/01/28

---
+++ sort_al_4.c
@@ -0,0 +1,42 @@
+//버블 정렬로 배열이 정렬을 마쳤는지 검사하기
+#include <stdio.h>
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define swap(type, x, y) do {type t = x; x = y; y = t;} while(0)
+
+//버블 정렬 응용
+int is_sorted(int a[], int n)
+{
+	int i;
+
+	for (i = n - 1; i > 0; i--)
+		if (a[i - 1] > a[i])
+			return 0;
+	return 1;
+}
+
+int main(void)
+{
+	int i, nx, sorted;
+	int *x;
+
+	puts("버블 정렬 응용(배열 정렬 검사)");
+	printf("요소수 : ");
+	scanf_s("%d", &nx);
+	x = calloc(nx, sizeof(int));
+
+	for (i = 0; i < nx; i++) {
+		printf("x[%d] : ", i);
+		scanf_s("%d", &x[i]);
+	}
+	sorted = is_sorted(x, nx);		//배열 x가 정렬되었는지 확인
+
+	if (sorted)
+		puts("배열이 정렬되어 있습니다.");
+	else
+		puts("배열이 정렬되어있지 않습니다.");
+
+	free(x);
+
+	return 0;
+}
